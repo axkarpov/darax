d3d57f4311b50412c2a519ff97d898f528984a3d
Alex Karpov <axkarpov@gmail.com>
2020-07-29 15:35:11 +0300

    Correct nav elements in header__top. Correct view for 320px - 800px. Create new block categories

--- dist/app/js/script.js
+++
@@ -1,7 +0,0 @@
-let user = document.getElementById('user');
-let user_block = document.getElementById('user_block');
-
-user.addEventListener("click", function (e) {
-    user_block.classList.toggle('open');
-});
-
